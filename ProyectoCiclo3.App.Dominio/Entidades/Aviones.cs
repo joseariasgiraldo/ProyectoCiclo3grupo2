86aea016822491c937a2c0184e48a6430ade2697
joseariasgiraldo <joseariasgiraldo@gmail.com>
2022-08-30 04:16:39 +0200

    firt comit

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
@@ -0,0 +1,8 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio{
+    public class Aviones{
+        public int id { get; set; }
+        public string modelo { get; set; }
+    }
+ 
+}
---
+++ ProyectoCiclo3.App.Persistencia/Migrations/20220823013853_CreacionTablaAviones.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoCiclo3.App.Persistencia.Migrations
+{
+    public partial class CreacionTablaAviones : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aviones",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    modelo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aviones", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aviones");
+        }
+    }
+}
